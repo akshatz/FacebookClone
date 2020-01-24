56f7d4c1b691abdb6802c8efd58f4637e7e917c9
Akshat Zala <akshatzala@gmail.com>
2020-01-24 12:53:54 +0530

    Changed search

--- users/views.py
+++ users/views.py
@@ -99,7 +99,7 @@ def search(request):
     if request.method == 'GET':
         query = request.GET.get('q')
         if query is not None and request.user:
-            results = User.objects.filter(Q(username=query) | Q(first_name=query) | Q(last_name=query))
+            results = User.objects.filter(Q(first_name=query) | Q(last_name=query))
             # print(request.user.id)
             return render(request, 'users/search.html', {'results': results, 'media': MEDIA_URL, "my_id":request.user.id})
         return render(request, 'base.html')
