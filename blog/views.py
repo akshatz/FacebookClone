7e81f1a67526201f3828e7030790bb1c00d73dab
Akshat Zala <akshatzala@gmail.com>
2020-01-26 21:22:23 +0530

    Added some changes

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def home_view(request):
     """Display all the post of friends and own posts on the dashboard"""
     if request.user.is_authenticated:
         context = {
-            'posts': Posts.objects.filter(author=request.user).order_by('-date_posted'),
+            'posts': Posts.objects.all().order_by('-date_posted'),
             'media': MEDIA_URL,
         }
         return render(request, 'blog/home.html', context)
@@ -131,4 +131,4 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(AUTH_USER_MODEL, username=self.kwargs.get('pk'))
-        return Posts.objects.filter(author=user).order_by('-date_posted')
+        return Posts.objects.all().order_by('-date_posted')
--- django_project/urls.py
+++ django_project/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('', user_views.home, name='home'),
+    # path('', user_views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True),
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
