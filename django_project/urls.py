c08b23762ae7c74bc9c07a94f6ee5365bb70c19b
Akshat Zala <akshatzala@gmail.com>
2020-02-01 20:45:54 +0530

    Added Password reset Feature

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,16 +24,28 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('', user_views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True),
-         name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
-         name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
+    path('login/', auth_views.LoginView.
+         as_view(template_name='users/login.html', redirect_authenticated_user=True),
+            name='login'),
+    path('logout/', auth_views.LogoutView.
+         as_view(template_name='users/logout.html'),
+            name='logout'),
+    path('password_reset/',
+         auth_views.PasswordResetView.
+         as_view(template_name='users/password_reset.html'),
+            name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.
+         as_view(template_name='users/password_reset_done.html'),
+            name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.
+         as_view(template_name='users/password_reset_confirm.html'),
+            name='password_reset_confirm'),
+    path('password_reset_confirm/',
+         auth_views.PasswordResetCompleteView
+         .as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('blog/', include('blog.urls')),
     path('users/', include('users.urls'), name='users'),
     path('friend/', include('friend.urls'), name='users'),
