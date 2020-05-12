0818c94f80ced5ab2519d0b388fcc9d5e0b28973
Akshat Zala <akshatzala@gmail.com>
2020-05-12 11:52:36 +0530

    Added database

--- django_project/settings.py
+++ django_project/settings.py
@@ -168,3 +168,6 @@ CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 django_heroku.settings(locals())
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
