c58b1cffc4f83476fd112fcfcabde6a3b5635206
Akshat Zala <akshatzala@gmail.com>
2020-05-12 09:07:17 +0530

    Added host

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['social-networking-django.herokuapp.com']
+ALLOWED_HOSTS = ['https://pacific-retreat-33369.herokuapp.com']
 
 # Application definition
 
