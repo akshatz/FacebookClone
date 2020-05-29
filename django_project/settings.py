9a1bc6cfa4fdf017fbda300202a17321a078df7b
Akshat Zala <akshatzala@gmail.com>
2020-05-29 13:03:30 +0530

    Change to all allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE")
 
-ALLOWED_HOSTS = ['vast-springs-06779.herokuapp']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
