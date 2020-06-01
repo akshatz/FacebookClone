08dba42e65f7b92b1dee90b6de6f6abd2a75bd06
Akshat Zala <akshatzala@gmail.com>
2020-06-01 14:10:53 +0530

    Changed Secret KEY

--- django_project/settings.py
+++ django_project/settings.py
@@ -150,8 +150,8 @@ LOGIN_URL = 'login'
 # EMAIL BACKENDS
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com' 
-EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_PASS")
+EMAIL_HOST_USER = 'akshatzala@gmail.com'
+EMAIL_HOST_PASSWORD = 'wbnbreznhcxxmzis'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_CONFIRMATION_PERIOD_DAYS = 7
