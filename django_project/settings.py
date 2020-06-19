b38172d2af4eb29c28d5649610a265232a3ba240
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2020-06-19 20:49:05 +0530

    Removed dj_database_url

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,5 +165,3 @@ AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME=os.getenv("AWS_STORAGE_BUCKET_NAME")
 
 django_heroku.settings(locals())
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
