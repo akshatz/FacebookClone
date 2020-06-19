6174858cb7d1573c6dac87bc547ba55459f9238a
Akshat Zala <akshatzala@gmail.com>
2020-06-19 12:18:08 +0530

    Removed comment from settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,5 +165,5 @@ AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
 django_heroku.settings(locals())
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
